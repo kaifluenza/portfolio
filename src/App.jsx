1cf43792eb1a33693bfc81a874fd8099b65d2855
kai34000 <wanrueni.12@gmail.com>
2024-05-16 17:59:07 -0400

    Build a NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hi. I'm Wanrueni.</h1>
-    <h2>A Front-End Developer.</h2>
-    </div>
-    
+   <NavBar/>
   )
 }
 
