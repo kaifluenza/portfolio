d077c8864abd07f3ac0e9121cc531a42e1666da2
kai34000 <wanrueni.12@gmail.com>
2024-05-21 14:06:41 -0400

    Add hover effect for project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,24 @@
 import { Image, Box, Text, Link, Button, VStack, Heading } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import React from 'react'
+import { useState } from 'react';
 
 
 const ProjectCard = ({project}) => {
+  const [hover, setHover] = useState(false);
+
   return (
     <Box 
     borderRadius={30}
     bgColor="gray.200" 
+    onMouseEnter={() => setHover(true)}
+    onMouseLeave={() => setHover(false)}
+    boxShadow={hover? "xl" : "none"}
     >
         <VStack spacing={4} h="100%" justifyContent="space-between" paddingY={4}>
-          <Text fontSize="3xl" fontWeight="bold">
+          <Text fontSize={hover? "3xl" : "2xl"} fontWeight="bold">
             <Link href={project.link} isExternal>{project.name}</Link>
           </Text>
       
-          <Box paddingX={8}>
+          <Box paddingX={hover? 6: 8}>
             <Image borderRadius={12} src={project.image_background} alt={project.name}/>
           </Box>
 
